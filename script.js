373e7f0a1964b7518dcc04a2107ead6f69afc757
xation222 <126095360+xation222@users.noreply.github.com>
2023-12-02 09:31:21 -0300

    numeros decimais

--- script.js
+++ script.js
@@ -7,12 +7,20 @@ calculo.addEventListener('keydown', function(e) { // evento Enter
 function calculando() { // função do calculo
     let result = eval(calculo.value); // criando uma variavel local com o valor resultado da conta
     const textResult = document.getElementById('res'); // selecionando elemento onde mostra resultado
-    if (result > 9999999) {
-        textResult.textContent = result.toExponential(2); // formatação para número cientifico para longas strings
+    if (result%1 != 0) {
+        if (result > 9999999) {
+            textResult.textContent = result.toExponential(2); // formatação para número cientifico para longas strings
+        } else {
+            textResult.textContent = result.toFixed(2); // formatação comum com dois numeros decimais
+        }
     } else {
-        textResult.textContent = result.toFixed(2); // formatação comum com dois numeros decimais
+        if (result > 9999999) {
+            textResult.textContent = result.toExponential(2); // formatação para número cientifico para longas strings
+        } else {
+            textResult.textContent = result; // formatação comum com dois numeros decimais
+        }
     }
-    historico(textResult.textContent);
+    historico(textResult.textContent, calculo.value);
     calculo.value = ""; // limpando area do texto
 }
 function botaoNumero(botao) { // função para adicionar texto na string por meio de botões
@@ -21,11 +29,11 @@ function botaoNumero(botao) { // função para adicionar texto na string por mei
 function apagar() { // função de apagar texto por botão
     calculo.value = calculo.value.slice(0, -1);
 }
-function historico(ultimaConta) {
+function historico(ultimaConta, operacao) { // função para histórico
     const historicoTexto = document.getElementById('historico');
     if (historicoTexto.textContent === "") {
-        historicoTexto.innerHTML += ultimaConta;
+        historicoTexto.innerHTML += operacao + " = " + ultimaConta;
     } else {
-        historicoTexto.innerHTML += "<br>" + ultimaConta;
+        historicoTexto.innerHTML += "<br>" + operacao + " = " + ultimaConta;
     }
 }
